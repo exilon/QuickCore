524e25f1277efb21666bfb119eb5188cfb6b1101
Exilon <kike@exilon.es>
2020-10-22 09:45:28 +0200

    [commandline] commandline parameters control

---
+++ Quick.Core.Commandline.pas
@@ -0,0 +1,75 @@
+{ ***************************************************************************
+
+  Copyright (c) 2016-2020 Kike Pérez
+
+  Unit        : Quick.Core.Commandline
+  Description : Core Commandline
+  Author      : Kike Pérez
+  Version     : 1.0
+  Created     : 29/07/2020
+  Modified    : 01/08/2020
+
+  This file is part of QuickCore: https://github.com/exilon/QuickCore
+
+ ***************************************************************************
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+ *************************************************************************** }
+
+unit Quick.Core.Commandline;
+
+{$i QuickCore.inc}
+
+interface
+
+uses
+  System.SysUtils,
+  Quick.Parameters;
+
+type
+  ICommandline<T : TParameters> = interface
+  ['{3773C294-CF56-451C-AE04-7BD3878BD633}']
+    function Value : T;
+  end;
+
+  TCommandline<T : TParameters> = class(TInterfacedObject,ICommandline<T>)
+  private
+    fValue : T;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    function Value : T;
+  end;
+
+implementation
+
+{ TCommandline<T> }
+
+constructor TCommandline<T>.Create;
+begin
+  fValue := T.Create;
+end;
+
+destructor TCommandline<T>.Destroy;
+begin
+  fValue.Free;
+  inherited;
+end;
+
+function TCommandline<T>.Value: T;
+begin
+  Result := fValue;
+end;
+
+end.
